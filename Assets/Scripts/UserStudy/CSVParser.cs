989066f81a28ad4ca7eb7268bfd5c744f21f62b6
Tamara Barounig <tamara.barounig@tum.de>
2018-02-19 11:27:51 +0100

    Included a CSVParser and started with implementing the UserStudyDataManager

---
+++ Assets/Scripts/UserStudy/CSVParser.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.IO;
+
+/**
+ * The following Code was inspired and partly copied from the following two sources:
+ * SUSHANTA CHAKRABORTY, "How to write data to CSV file in UNITY", February 20 2015, https://sushanta1991.blogspot.de/2015/02/how-to-write-data-to-csv-file-in-unity.html
+ * shanemartin2797, "HOW TO READ FROM AND WRITE TO CSV IN UNITY", November 20, 2015, https://shanemartin2797blog.wordpress.com/2015/11/20/how-to-read-from-and-write-to-csv-in-unity/
+ * 
+ **/
+
+public class CSVParser : MonoBehaviour {
+
+    #region Private variables
+    private StreamWriter _writer = null;
+
+    private bool _csvAlreadyThere = false;
+
+    private int _numberCSVColumns = 0;
+    private int _writtenRowData = 0;
+
+    private string _fileName;
+    #endregion
+
+    /** 
+     * Instanciates the CSV parser 
+     **/
+    public CSVParser(string file)
+    {
+        _fileName = file;
+    }
+
+
+    /**
+     * Instanciates the file writer and get's the path to the output file
+     **/
+    public void Start()
+    {
+        string path = getPath(_fileName);
+        _csvAlreadyThere = File.Exists(path);
+        _writer = new StreamWriter(path, true);
+    }
+
+    /**
+     * Writes the header of the CSV file if the file is emtpy.
+     * Note that the fields must be seperated by comma.
+     **/
+    public void writeHeader(string header, int numberColumns)
+    {
+        _numberCSVColumns = numberColumns;
+        if (!_csvAlreadyThere)
+        {
+            _writer.Write(header);
+            _writer.Flush();
+        }
+    }
+
+    /**
+     * Add new values to the file and automatically starts a new line if as many values were added as the spezified header has columns.
+     * If the flag finishedWriting is set to true, the writer is closed.
+     **/
+    public bool appendValues(string value, int number = 1, bool finishedWriting = false)
+    {
+        if (_writtenRowData == 0)
+        {
+            _writer.Write("\r\n" + value);
+        }
+        else
+        {
+            _writer.Write("," + value);
+        }
+        _writer.Flush();
+
+        // Sets the number of written data to 0 if as many values where written as the header contains columns
+        // Otherwise the number of written rows is updated
+        _writtenRowData = ((_writtenRowData + number) == _numberCSVColumns) ? 0 : (_writtenRowData + number);
+
+        if (finishedWriting)
+        {
+            _writer.Close();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Use Platform #define directives to retrieve the path for the CSV file depending on the used platform.
+     **/
+    private string getPath(string file)
+    {
+        #if UNITY_EDITOR
+            // If the directory CSV doesn't exist in Assets yet, it is created
+            if (!Directory.Exists(Application.dataPath + "/CSV/"))
+                {
+                 Directory.CreateDirectory(Application.dataPath + "/CSV/");
+                }
+            return Application.dataPath + "/CSV/" + file;
+        #else
+            return Application.dataPath +"/"+file;
+        #endif
+
+    }
+
+}
--- Assets/Scripts/UserStudy/UserStudyDataManager.cs
+++ Assets/Scripts/UserStudy/UserStudyDataManager.cs
@@ -7,6 +7,31 @@ public static class UserStudyDataManager {
     #region PRIVATE_MEMBER_VARIABLES
 
     /// <summary>
+    /// Parser to store the survey data into a CSV
+    /// </summary>
+    private static CSVParser _parser = null;
+
+    /// <summary>
+    /// The maximal number of levels used in this user study.
+    /// </summary>
+    private static int _maxLevelCount = 5;
+
+    /// <summary>
+    /// Integer identifying the selected starting method of the study.
+    /// </summary>
+    private static int _startingMethod = 0;
+
+    /// <summary>
+    /// Integer representing the control method of the actual level.
+    /// </summary>
+    private static int _currentMethod = 0;
+
+    /// <summary>
+    /// Integer representing the current level of the user study.
+    /// </summary>
+    private static int _currentLevel = 0;
+
+    /// <summary>
     /// Time when the avatar leaves the start tracking area.
     /// </summary>
     private static float _startTime = 0;
@@ -19,11 +44,116 @@ public static class UserStudyDataManager {
     /// <summary>
     /// The identifier is a 7 digit code which is used to uniquely and anonymously identify the user.
     /// </summary>
-    private static string _identifier;
+    private static string _identifier = "";
+
+    /// <summary>
+    /// The key used in the PlayerPrefs to store the user's identifier.
+    /// </summary>
+    private static string _keyIdentifier = "userID";
+
+    /// <summary>
+    /// The key used in the PlayerPrefs to store the start control method.
+    /// </summary>
+    private static string _keyStartControl = "startContr";
+
+    /// <summary>
+    /// The key used in the PlayerPrefs to store the current level.
+    /// </summary>
+    private static string _keyCurrentLevel = "level";
 
     #endregion
 
+    // The static constructor is called when the game starts
+    static UserStudyDataManager()
+    {
+        // Initialize the CSV parser
+        _parser = new CSVParser("Saved_Data.csv");
+        _parser.Start();
 
+        // Write header of the CSV file if file not already exists
+        _parser.writeHeader("Identifier,order,stage,method,time", 5);
+
+        // Read information from PlayerPrefs if available
+        if (PlayerPrefs.HasKey(_keyIdentifier))
+        {
+            // Set identifier
+            _identifier = PlayerPrefs.GetString(_keyIdentifier, "");
+
+            if (PlayerPrefs.HasKey(_keyStartControl))
+            {
+                _startingMethod = PlayerPrefs.GetInt(_keyStartControl);
+            }
+            if (PlayerPrefs.HasKey(_keyCurrentLevel))
+            {
+                _currentLevel = PlayerPrefs.GetInt(_keyCurrentLevel);
+            }
+
+            // Set the current control method according to: ((level % 2) + startMethod) % 2
+            _currentMethod = ((_currentLevel % 2) + _startingMethod) % 2;
+        }
+        else
+        {
+            // Determine starting control method
+            // TODO
+        }
+
+    }
+    
+    /// <summary>
+    /// Tells if the identifier is still needed or already available due to former levels.
+    /// </summary>
+    /// <returns></returns>
+    public static bool identifierNeeded()
+    {
+        return _identifier.Equals("");
+    }
+
+    /// <summary>
+    /// Sets the user's identifier to the given one if not already set.
+    /// </summary>
+    /// <param name="userID">A 7 digit code representing the user's identifier.</param>
+    public static void setIdentifier(string userID)
+    {
+        if (_identifier.Equals(""))
+        {
+            _identifier = userID;
+        }
+    }
+
+    /// <summary>
+    /// Returns the control method of the actual level.
+    /// </summary>
+    /// <returns></returns>
+    public static int getCurrentControlMethod()
+    {
+        return _currentMethod;
+    }
+
+    /// <summary>
+    /// This method stores the time data and user information into the CSV file.
+    /// If this was not the last level of the survey, the user information are also stored into the PlayerPrefs.
+    /// </summary>
+    public static void endSurveyPart()
+    {
+        // Write to CSV
+        _parser.appendValues(_identifier + "," + _currentLevel + "," + (int)(_currentLevel / 2) + "," + _currentMethod + "," + (_endTime - _startTime), 5, true);
+
+        // Wrtie to PlayerPrefs
+        if(_currentLevel < _maxLevelCount)
+        {
+            if(_currentLevel == 0)
+            {
+                PlayerPrefs.SetString(_keyIdentifier, _identifier);
+                PlayerPrefs.SetInt(_keyStartControl, (_startingMethod));
+            }
+            PlayerPrefs.SetInt(_keyCurrentLevel, (_currentLevel + 1));
+        }
+        else
+        {
+            PlayerPrefs.DeleteAll();
+        }
+        
+    }
 
     #region Time tracking
     /// <summary>
@@ -40,7 +170,6 @@ public static class UserStudyDataManager {
     public static void endTimeTracking()
     {
         _endTime = Time.time;
-        Debug.Log(_startTime - _endTime);
     }
     #endregion 
 
