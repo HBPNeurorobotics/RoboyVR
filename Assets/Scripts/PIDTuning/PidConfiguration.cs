d87ab874b3cd71b822e10e13c3c97dd75b0ee2c2
Markus Webel <m@rkus.online>
2019-08-22 17:10:40 +0200

    Data-only classes for PID parameter storage

---
+++ Assets/Scripts/PIDTuning/PidConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace PIDTuning
+{
+    /// <summary>
+    /// A mapping from joint names to PID parameters
+    /// Provides facilities for (de)serialization
+    /// </summary>
+    public class PidConfiguration
+    {
+        public readonly string Name;
+
+        public readonly DateTime CreatedTimestamp;
+
+        public readonly Dictionary<string, PidParameters> Parameters;
+
+        public PidConfiguration(string name)
+        {
+            Name = name;
+            CreatedTimestamp = DateTime.Now.ToUniversalTime();
+            Parameters = new Dictionary<string, PidParameters>();
+        }
+
+        public void InitializePidParameters(IEnumerable<string> jointNames, PidParameters initializeValue)
+        {
+            // We need to make sure that noone changes PidParameters from a struct to a class
+            // because the implicit copy-on-assign below will not work with classes
+            Assert.IsTrue(typeof(PidParameters).IsValueType);
+
+            foreach (var name in jointNames)
+            {
+                Parameters[name] = initializeValue;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PIDTuning/PidParameters.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PIDTuning
+{
+    /// <summary>
+    /// Gain parameters for a PID controller
+    /// Available in both standard and parallel form
+    /// </summary>
+    public struct PidParameters
+    {
+        public readonly double Kp, Ki, Kd;
+
+        public double Ti
+        {
+            get { return Kp / Ki; }
+        }
+
+        public double Td
+        {
+            get { return Kd / Kp; }
+        }
+
+        private PidParameters(double kp, double ki, double kd)
+        {
+            Kp = kp;
+            Ki = ki;
+            Kd = kd;
+        }
+
+        public static PidParameters FromParallelForm(double kp, double ki, double kd)
+        {
+            return new PidParameters(kp, ki, kd);
+        }
+
+        public static PidParameters FromStandardForm(double kp, double ti, double td)
+        {
+            return new PidParameters(
+                kp,
+                kp / ti,
+                kp * td
+            );
+        }
+    }
+}
